68abb7c3d22c1641e2071149cd66c21dcdc07d77
Bruz Marzolf <bruz@bruzilla.com>
2022-04-21 10:08:39 -0700

    Add vix and buildpacks to support it

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule NifTest.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.1"}
+      {:distillery, "~> 2.1"},
+      {:vix, "~> 0.8.0"}
     ]
   end
 
